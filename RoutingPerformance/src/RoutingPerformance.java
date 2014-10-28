b57f82055ee8c25ba1402ee19bd661896c2333c1
Justin K Nam <justin.k.nam@gmail.com>
2014-10-28 12:57:23 +1100

    Getting started
    Created some classes and stuff - not done.

--- RoutingPerformance/src/RoutingPerformance.java
+++ RoutingPerformance/src/RoutingPerformance.java
@@ -1,4 +1,64 @@
 
 public class RoutingPerformance {
+	public static void main(String[] args){
+		
+		NetworkScheme nScheme;
+		RoutingScheme rScheme;
+		
+		if (args.length > 0) {
+		    try {
+		    	
+		    	//Choose networking scheme
+		        if(args[0].equals("CIRCUIT")){
+		        	nScheme = new Circuit();
+		        } else if(args[0].equals("PACKET")) {
+		        	nScheme = new Packet();
+		        } else {
+		        	//end program
+		        }
+		        
+		        //Choose routing scheme
+		        if(args[1].equals("SHP")){
+		        	rScheme = new SHP();
+		        } else if(args[1].equals("SDP")) {
+		        	rScheme = new SDP();
+		        } else if(args[1].equals("LLP")) {
+		        	rScheme = new LLP();
+		        } else {
+		        	//end program
+		        }
+		    } catch (NumberFormatException e) {
+		        System.err.println("Argument" + args[0] + " must be an integer.");
+		        System.exit(1);
+		    }
+		}
+	}
+}
 
+interface NetworkScheme {
+
+}
+
+interface RoutingScheme {
+	
+}
+
+private class Circuit implements NetworkScheme{
+	
+}
+
+private class Packet implements NetworkScheme{
+	
+}
+
+private class SHP implements RoutingScheme {
+	
+}
+
+private class SDP implements RoutingScheme{
+	
+}
+
+private class LLP implements RoutingScheme{
+	
 }
