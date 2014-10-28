62c5ca7ea07cfb74bf3720d82edfd2056ed83c5a
Justin Nam <justin.k.nam@gmail.com>
2014-10-28 17:29:59 +1100

    SCREW YOU ECLIPSE

--- RoutingPerformance/src/RoutingPerformance.java
+++ RoutingPerformance/src/RoutingPerformance.java
@@ -1,9 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
-
-<<<<<<< HEAD
-
 public class RoutingPerformance {
 	public static void main(String[] args){
 		NetworkingScheme nScheme;
@@ -86,70 +83,3 @@ public class RoutingPerformance {
 	public static final int WORKLOAD_FILE = 3;
 	public static final int PACKET_RATE = 4;
 }
-
-
-=======
-public class RoutingPerformance {
-	public static void main(String[] args){
-		
-		NetworkScheme nScheme;
-		RoutingScheme rScheme;
-		
-		if (args.length > 0) {
-		    try {
-		    	
-		    	//Choose networking scheme
-		        if(args[0].equals("CIRCUIT")){
-		        	nScheme = new Circuit();
-		        } else if(args[0].equals("PACKET")) {
-		        	nScheme = new Packet();
-		        } else {
-		        	//end program
-		        }
-		        
-		        //Choose routing scheme
-		        if(args[1].equals("SHP")){
-		        	rScheme = new SHP();
-		        } else if(args[1].equals("SDP")) {
-		        	rScheme = new SDP();
-		        } else if(args[1].equals("LLP")) {
-		        	rScheme = new LLP();
-		        } else {
-		        	//end program
-		        }
-		    } catch (NumberFormatException e) {
-		        System.err.println("Argument" + args[0] + " must be an integer.");
-		        System.exit(1);
-		    }
-		}
-	}
-}
-
-interface NetworkScheme {
-
-}
-
-interface RoutingScheme {
-	
-}
-
-private class Circuit implements NetworkScheme{
-	
-}
-
-private class Packet implements NetworkScheme{
-	
-}
-
-private class SHP implements RoutingScheme {
-	
-}
-
-private class SDP implements RoutingScheme{
-	
-}
-
-private class LLP implements RoutingScheme{
-	
-}
->>>>>>> b57f82055ee8c25ba1402ee19bd661896c2333c1
